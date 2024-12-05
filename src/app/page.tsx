1914169fd203007873b6ec5d0f3d52bc00daf606
chesterosoronyas <conyasetia@gmail.com>
2024-12-05 16:02:20 +0300

    Added in next-auth and items table

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,40 @@
 import Image from "next/image";
-import  {database} from "@/db/database";
-import { bids as bidsSchema } from "@/db/schema";
+import { database } from "@/db/database";
+import { bids as bidsSchema, items } from "@/db/schema";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { revalidatePath } from "next/cache";
+import SignIn from "@/components/sign-in";
+import { SignOut } from "@/components/sign-out";
+import { auth } from "@/auth";
 export default async function HomePage() {
+  const session = await auth();
 
-  const bids=await database.query.bids.findMany();
+  if (!session) return null;
+  if (!session.user) return null;
+  const allitems = await database.query.items.findMany();
   return (
     <main className="container mx-auto py-12">
-      <form action={async(formData:FormData)=>{
-        'use server';
-        //const bid=formData.get('bid') as string;
-        await database.insert(bidsSchema).values({});
-        revalidatePath('/'); 
-      }}>
-        <Input name="bid"  placeholder="Bid"/>
-        <Button type="submit">Place bid</Button>
+      {session ? <SignOut /> : <SignIn />}
+
+      <form
+        action={async (formData: FormData) => {
+          "use server";
+          //const bid=formData.get('bid') as string;
+          await database.insert(items).values({
+            name: formData.get("name") as string,
+            userId: session?.user?.id!,
+          });
+          revalidatePath("/");
+        }}
+      >
+        <Input name="name" placeholder="Name your item" />
+        <Button type="submit">Post Item</Button>
       </form>
 
-      {
-        bids.map(bid=>(
-          <div key={bid.id}>{
-          bid.id}</div>
-        ))
-      }
+      {allitems.map((item) => (
+        <div key={item.id}>{item.name}</div>
+      ))}
     </main>
   );
 }
